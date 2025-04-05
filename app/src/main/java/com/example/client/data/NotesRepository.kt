51b427abe8f5649a87f66b43b01959a23eec3780
Marcos <marcos.sanchez149@educa.madrid.org>
2025-04-05 11:32:10 +0200

    Creación screen add note 1

--- app/src/main/java/com/example/client/data/NotesRepository.kt
+++ app/src/main/java/com/example/client/data/NotesRepository.kt
@@ -1,4 +1,10 @@
 package com.example.client.data
 
-class NotesRepository {
+import com.example.client.domain.model.note.Note
+import javax.inject.Inject
+
+class NotesRepository @Inject constructor(
+    private val notes: notesDataSource,
+) {
+    fun addNote(note: Note) = notes.insertNotes(note)
 }
