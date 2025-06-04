e52f4eb5910c5c1ded2b874f43349d20ae7449f7
Marcos <marcos.sanchez149@educa.madrid.org>
2025-06-04 11:38:57 +0200

    Creacion OrderNotesByChronologicalUseCase flow

---
+++ app/src/main/java/com/example/client/domain/useCases/note/OrderNotesByChronologicalUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.client.domain.useCases.note
+
+import com.example.client.data.repositories.NoteRepository
+import javax.inject.Inject
+
+class OrderNotesByChronologicalUseCase @Inject constructor(private val noteRepository: NoteRepository) {
+    suspend operator fun invoke() = noteRepository.orderByChronologicalOrder()
+}
