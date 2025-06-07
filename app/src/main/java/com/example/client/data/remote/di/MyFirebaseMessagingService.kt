4e4db83dff3232221d31e250dc1b9ee2dda39543
Marcos <marcos.sanchez149@educa.madrid.org>
2025-06-07 20:37:31 +0200

    Arreglos pero todavia no funcional

--- app/src/main/java/com/example/client/data/remote/di/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/client/data/remote/di/MyFirebaseMessagingService.kt
@@ -1,7 +1,18 @@
 package com.example.client.data.remote.di
 
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import android.util.Log
 
-class MyFirebaseMessagingService : FirebaseMessagingService()  {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // Manejar el mensaje recibido
+        Log.d("FirebaseMessaging", "Mensaje recibido: ${remoteMessage.data}")
+    }
+
+    override fun onNewToken(token: String) {
+        // Manejar la actualización del token
+        Log.d("FirebaseMessaging", "Nuevo token: $token")
+    }
 }
